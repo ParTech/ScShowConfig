f1a061f066747d33e2251768ebce30df92b80e64
Ruud van Falier <ruud@partechit.nl>
2016-07-04 13:11:50 +0200

    Initial commit

---
+++ ScShowConfig/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace ScShowConfig
+{
+    using System;
+    using System.IO;
+    using System.Windows.Forms;
+    using Sitecore;
+    using Sitecore.Configuration;
+
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a Sitecore App_Config folder to continue...");
+
+            // Select App_Config folder
+            string appConfigFolderPath = SelectFolder();
+
+            if (appConfigFolderPath == null)
+            {
+                Console.WriteLine("No folder selected... Abort!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Copy the folder to our application path.
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            string targetAppConfigFolderPath = Path.Combine(root, "App_Config");
+
+            if (Directory.Exists(targetAppConfigFolderPath))
+            {
+                Directory.Delete(targetAppConfigFolderPath, true);
+            }
+
+            CopyFilesRecursively(new DirectoryInfo(appConfigFolderPath), new DirectoryInfo(targetAppConfigFolderPath));
+
+            // Verify that we have a Sitecore.config
+            if (!File.Exists(Path.Combine(targetAppConfigFolderPath, "Sitecore.config")))
+            {
+                Console.WriteLine("Sitecore.config does not exist in App_Config... Abort!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Load the configuration
+            Context.Items["sc_IsUnitTesting"] = true;
+            State.HttpRuntime.AppDomainAppPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            var xml = Sitecore.Configuration.Factory.GetConfiguration();
+
+            // Save and quit.
+            xml.Save("output.xml");
+            
+            Console.WriteLine("Saved consolidated configuration to output.xml");
+            Console.ReadLine();
+        }
+
+        private static string SelectFolder()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            
+            if (!string.IsNullOrWhiteSpace(dialog.SelectedPath))
+            {
+                return dialog.SelectedPath;
+            }
+
+            return null;
+        }
+
+        private static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            }
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
+    }
+}
---
+++ ScShowConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScShowConfig")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ParTech IT B.V.")]
+[assembly: AssemblyProduct("ScShowConfig")]
+[assembly: AssemblyCopyright("Copyright © ParTech IT B.V. 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e29f35f3-f36f-49b0-97ac-78c96bb53f3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
